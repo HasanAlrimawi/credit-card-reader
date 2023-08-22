0e754eff1597bfe8b2f6e354694e94be35153d27
HasanAlrimawi <hasan.alrimawi@exalt.ps>
2023-08-22 16:40:43 +0300

    corrected wrong import directory

--- services/card-reader-extractor.js
+++ services/card-reader-extractor.js
@@ -1,4 +1,4 @@
-import { CardInformation } from "../objects-templates/card-information.js";
+import { CardInformation } from "../models/card-information.js";
 
 export const cardDetailsService = (function () {
   function extractCardDetails(readStripe) {
