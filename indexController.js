24622b8968a114fde7561f036a7a638e7c697e2e
HasanAlrimawi <hasan.alrimawi@exalt.ps>
2023-09-13 12:28:13 +0300

    enhanced code for generating color picker

--- indexController.js
+++ indexController.js
@@ -33,17 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     barcodeScannerController.finalizeWork();
   });
 
-  document
-    .getElementById("background-color-picker")
-    .addEventListener("change", updateBackgroundColor);
-
-  document
-    .getElementById("buttons-color-picker")
-    .addEventListener("change", updateButtonsColor);
-
-  document
-    .getElementById("peripherals-control-color-picker")
-    .addEventListener("change", updatePeripheralsControColor);
+  renderColorPickers();
 });
 
 /**
@@ -66,24 +56,97 @@ function showDevice(deviceController) {
   peripheralsTagControl.highlightPeripheralSelected(deviceController.myId);
 }
 
-const updateBackgroundColor = function () {
-  const selectedColor = document.getElementById(
-    "background-color-picker"
-  ).value;
+/**
+ * Calls the function needed to append the color pickers after defining
+ *     the color pickers needed accompanied with their event listeners.
+ */
+const renderColorPickers = function () {
+  const colorPickers = [
+    {
+      title: "Background color:",
+      id: "background-color-picker",
+      changeEventHandler: () => {
+        updateBackgroundColor("background-color-picker");
+      },
+    },
+    {
+      title: "Buttons color:",
+      id: "buttons-color-picker",
+      changeEventHandler: () => {
+        updateButtonsColor("buttons-color-picker");
+      },
+    },
+    {
+      title: "Peripherals control color:",
+      id: "peripherals-control-color-picker",
+      changeEventHandler: () => {
+        updatePeripheralsControlColor("peripherals-control-color-picker");
+      },
+    },
+  ];
+
+  for (const picker of colorPickers) {
+    addColorPicker(picker.title, picker.id);
+    document
+      .getElementById(picker.id)
+      .addEventListener("change", picker.changeEventHandler);
+  }
+};
+
+/**
+ * Adds a color picker with its label to the dropdown list
+ *     for selecting the preferred color.
+ * @param {string} pickerTitle Represents the name of
+ *     what will get colored upon selection
+ * @param {string} pickerId Represents the id of the element that will be colored
+ */
+const addColorPicker = function (pickerTitle, pickerId) {
+  const dropdownContentDiv = document.getElementById("dropdown-content-holder");
+  const rowElementsHolderDiv = document.createElement("div");
+  const pickerTitleLabel = document.createElement("label");
+  const colorPickerInput = document.createElement("input");
+
+  rowElementsHolderDiv.setAttribute("class", "dropdown-elements form-row");
+  pickerTitleLabel.setAttribute("for", pickerId);
+  pickerTitleLabel.textContent = pickerTitle;
+  colorPickerInput.setAttribute("type", "color");
+  colorPickerInput.setAttribute("name", pickerId);
+  colorPickerInput.setAttribute("id", pickerId);
+
+  rowElementsHolderDiv.appendChild(pickerTitleLabel);
+  rowElementsHolderDiv.appendChild(colorPickerInput);
+  dropdownContentDiv.appendChild(rowElementsHolderDiv);
+};
+
+/**
+ * Will reset the color of the background as selected by the user.
+ * @param {string} colorPickerId Represents the id of the element that
+ *     will be colored
+ */
+const updateBackgroundColor = function (colorPickerId) {
+  const selectedColor = document.getElementById(colorPickerId).value;
   const root = document.querySelector(":root");
   root.style.setProperty("--main-color", selectedColor);
 };
 
-const updateButtonsColor = function () {
-  const selectedColor = document.getElementById("buttons-color-picker").value;
+/**
+ * Will reset the color of the background as selected by the user.
+ * @param {string} colorPickerId Represents the id of the element that
+ *     will be colored
+ */
+const updateButtonsColor = function (colorPickerId) {
+  const selectedColor = document.getElementById(colorPickerId).value;
   const root = document.querySelector(":root");
   root.style.setProperty("--button-color", selectedColor);
 };
 
-const updatePeripheralsControColor = function () {
-  const selectedColor = document.getElementById(
-    "peripherals-control-color-picker"
-  ).value;
+/**
+ * Will reset the color of the background as selected by the user.
+ * @param {string} colorPickerId Represents the id of the element that
+ *     will be colored
+ */
+const updatePeripheralsControlColor = function (colorPickerId) {
+  const selectedColor = document.getElementById(colorPickerId).value;
   const root = document.querySelector(":root");
   root.style.setProperty("--white-to-black", selectedColor);
 };
